6fece68322a40de77acb45bc24a0abc5a12aa661
philipmeliga <philipmeliga01@gmail.com>
2026-05-09 19:04:40 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ const apiKey = "34c688a1fa17e26a2a16e12f3ea9b4c3";
 const newsContainer = document.getElementById("newsContainer");
 const loader = document.getElementById("loader");
 
-
-// FETCH NEWS
 async function getNews(){
 
     loader.style.display = "block";
@@ -17,8 +15,7 @@ async function getNews(){
     document.getElementById("country").value;
 
     let url =
-`https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}`;
-
+`https://gnews.io/api/v4/{endpoint}?{parameters}&apikey=apiKey}`;
 
     try{
 
@@ -26,9 +23,21 @@ async function getNews(){
 
         const data = await response.json();
 
+        console.log(data);
+
         loader.style.display = "none";
 
-        if(data.articles.length === 0){
+        // CHECK FOR API ERROR
+        if(data.status !== "ok"){
+
+            newsContainer.innerHTML =
+            `<h2>${data.message}</h2>`;
+
+            return;
+        }
+
+        // CHECK EMPTY ARTICLES
+        if(!data.articles || data.articles.length === 0){
 
             newsContainer.innerHTML =
             "<h2>No news found.</h2>";
@@ -77,87 +86,11 @@ async function getNews(){
         loader.style.display = "none";
 
         newsContainer.innerHTML =
-        "<h2>Error loading news.</h2>";
+        `<h2>${error.message}</h2>`;
 
         console.log(error);
 
     }
 }
 
-
-// SEARCH NEWS
-async function searchNews(){
-
-    const search =
-    document.getElementById("searchInput").value;
-
-    if(search === ""){
-        return;
-    }
-
-    loader.style.display = "block";
-    newsContainer.innerHTML = "";
-
-    let url =
-`https://newsapi.org/v2/everything?q=${search}&apiKey=${apiKey}`;
-
-
-    try{
-
-        const response = await fetch(url);
-
-        const data = await response.json();
-
-        loader.style.display = "none";
-
-        data.articles.forEach(article => {
-
-            const card = document.createElement("div");
-
-            card.classList.add("news-card");
-
-            card.innerHTML = `
-
-                <img src="${
-                    article.urlToImage ||
-                    'https://via.placeholder.com/300'
-                }">
-
-                <div class="news-content">
-
-                    <h2>${article.title}</h2>
-
-                    <p>
-                        ${
-                            article.description ||
-                            "No description available."
-                        }
-                    </p>
-
-                    <a href="${article.url}"
-                    target="_blank">
-                    Read More
-                    </a>
-
-                </div>
-            `;
-
-            newsContainer.appendChild(card);
-
-        });
-
-    }catch(error){
-
-        loader.style.display = "none";
-
-        newsContainer.innerHTML =
-        "<h2>Error searching news.</h2>";
-
-        console.log(error);
-
-    }
-}
-
-
-// LOAD DEFAULT NEWS
 getNews();
